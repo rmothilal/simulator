765aaa5083bd292ed6e77c7fa7b931457f9e4823
Rajiv Mothilal <rajivmothilal@gmail.com>
2020-03-13 00:36:21 +0200

    added endpoint POST transfers for Payer

--- src/payer/handler.js
+++ src/payer/handler.js
@@ -30,6 +30,14 @@ const callbackCache = new NodeCache()
 const Metrics = require('../lib/metrics')
 const Logger = require('@mojaloop/central-services-logger')
 const Enums = require('@mojaloop/central-services-shared').Enum
+const base64url = require('base64url')
+const https = require('https')
+const sendRequest = require('../lib/sendRequest')
+
+const transfersEndpoint = process.env.TRANSFERS_ENDPOINT || 'http://localhost:1080'
+const transfersFulfilResponseDisabled = (process.env.TRANSFERS_FULFIL_RESPONSE_DISABLED !== undefined && process.env.TRANSFERS_FULFIL_RESPONSE_DISABLED !== 'false')
+const transfersFulfilment = process.env.TRANSFERS_FULFILMENT || 'XoSz1cL0tljJSCp_VtIYmPNw-zFUgGfbUqf69AagUzY'
+const signature = process.env.MOCK_JWS_SIGNATURE || 'abcJjvNrkyK2KBieDUbGfhaBUn75aDUATNF4joqA8OLs4QgSD7i6EO8BIdy6Crph3LnXnTM20Ai1Z6nt0zliS_qPPLU9_vi6qLb15FOkl64DQs9hnfoGeo2tcjZJ88gm19uLY_s27AJqC1GH1B8E2emLrwQMDMikwQcYvXoyLrL7LL3CjaLMKdzR7KTcQi1tCK4sNg0noIQLpV3eA61kess'
 
 const extractUrls = (request) => {
   const urls = {}
@@ -153,6 +161,117 @@ exports.putQuotesByIdAndError = function (request, h) {
   callbackCache.set(request.params.id, incomingRequest)
 
   return h.response().code(Enums.Http.ReturnCodes.OK.CODE)
+}
+
+exports.postTransfers = async function (request, h) {
+  const histTimerEnd = Metrics.getHistogram(
+    'sim_request',
+    'Histogram for Simulator http operations',
+    ['success', 'fsp', 'operation', 'source', 'destination']
+  ).startTimer()
+
+  Logger.debug(`[cid=${request.payload.transferId}, fsp=${request.headers['fspiop-source']}, source=${request.headers['fspiop-source']}, dest=${request.headers['fspiop-destination']}] ~ Simulator::api::payer::postTransfers - START`)
+
+  const metadata = `${request.method} ${request.path} ${request.payload.transferId}`
+  Logger.info(`IN PAYERFSP:: received: ${metadata}.`)
+
+  if (!transfersFulfilResponseDisabled) {
+    // Saving Incoming request
+    const incomingRequest = {
+      headers: request.headers,
+      data: request.payload
+    }
+    requestCache.set(request.payload.transferId, incomingRequest)
+
+    const url = transfersEndpoint + '/transfers/' + request.payload.transferId
+    const fspiopUriHeader = `/transfers/${request.payload.transferId}`
+    try {
+      const transfersResponse = {
+        // fulfilment: "rjzWyHf4IUao60Yz98HZOIhZbqtclOgZ7WriZuq9Hn0",
+        fulfilment: transfersFulfilment,
+        completedTimestamp: new Date().toISOString(),
+        transferState: 'COMMITTED'
+      }
+      const protectedHeader = {
+        alg: 'RS256',
+        'FSPIOP-Source': `${request.headers['fspiop-destination']}`,
+        'FSPIOP-Destination': `${request.headers['fspiop-source']}`,
+        'FSPIOP-URI': `/transfers/${request.payload.transferId}`,
+        'FSPIOP-HTTP-Method': 'PUT',
+        Date: ''
+      }
+      const fspiopSignature = {
+        signature: signature,
+        protectedHeader: `${base64url.encode(JSON.stringify(protectedHeader))}`
+      }
+      const opts = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/vnd.interoperability.transfers+json;version=1.0',
+          'FSPIOP-Source': request.headers['fspiop-destination'],
+          'FSPIOP-Destination': request.headers['fspiop-source'],
+          Date: new Date().toUTCString(),
+          'FSPIOP-Signature': JSON.stringify(fspiopSignature),
+          'FSPIOP-HTTP-Method': 'PUT',
+          'FSPIOP-URI': fspiopUriHeader
+          // traceparent: request.headers.traceparent ? request.headers.traceparent : undefined,
+          // tracestate: request.headers.tracestate ? request.headers.tracestate : undefined
+        },
+        transformRequest: [(data, headers) => {
+          delete headers.common.Accept
+          return data
+        }],
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false
+        }),
+        data: JSON.stringify(transfersResponse)
+      }
+
+      // Logger.info(`Executing PUT: [${url}], HEADERS: [${JSON.stringify(opts.headers)}], BODY: [${JSON.stringify(transfersResponse)}]`)
+      const res = await sendRequest(url, opts, request.span)
+      // Logger.info(`response: ${res.status}`)
+      if ((res.status !== Enums.Http.ReturnCodes.ACCEPTED.CODE) || (res.status !== Enums.Http.ReturnCodes.OK.CODE)) {
+        // TODO: how does one identify the failed response?
+        throw new Error(`Failed to send. Result: ${JSON.stringify(res)}`)
+      }
+      // Logger.perf(`[cid=${request.payload.transferId}, fsp=${request.headers['fspiop-source']}, source=${request.headers['fspiop-source']}, dest=${request.headers['fspiop-destination']}] ~ Simulator::api::payer::postTransfers - END`)
+      histTimerEnd({
+        success: true,
+        fsp: 'payer',
+        operation: 'postTransfers',
+        source: request.headers['fspiop-source'],
+        destination: request.headers['fspiop-destination']
+      })
+    } catch (err) {
+      Logger.error(err)
+      // Logger.perf(`[cid=${request.payload.transferId}, fsp=${request.headers['fspiop-source']}, source=${request.headers['fspiop-source']}, dest=${request.headers['fspiop-destination']}] ~ Simulator::api::payer::postTransfers - ERROR`)
+      histTimerEnd({
+        success: false,
+        fsp: 'payer',
+        operation: 'postTransfers',
+        source: request.headers['fspiop-source'],
+        destination: request.headers['fspiop-destination']
+      })
+      // TODO: what if this fails? We need to log. What happens by default?
+      // const url = await rq.createErrorUrl(db, request.path, requesterName);
+      // TODO: review this error message
+      // TODO: we should be able to throw an AppError somewhere, test whether the error
+      // received in this handler is an AppError, then send the requester the correct
+      // payload etc. based on the contents of that AppError.
+      // rq.sendError(url, asyncResponses.serverError, rq.defaultHeaders(requesterName, 'participants'), {logger});
+    }
+  } else {
+    // Logger.perf(`[cid=${request.payload.transferId}, fsp=${request.headers['fspiop-source']}, source=${request.headers['fspiop-source']}, dest=${request.headers['fspiop-destination']}] ~ Simulator::api::payer::postTransfers - END`)
+    histTimerEnd({
+      success: true,
+      fsp: 'payer',
+      operation: 'postTransfers',
+      source: request.headers['fspiop-source'],
+      destination: request.headers['fspiop-destination']
+    })
+  }
+
+  return h.response().code(Enums.Http.ReturnCodes.ACCEPTED.CODE)
 }
 
 // Section about Transfers
--- src/payer/routes.js
+++ src/payer/routes.js
@@ -143,6 +143,57 @@ module.exports = [
     }
   },
   {
+    method: 'POST',
+    path: '/payerfsp/transfers',
+    handler: Handler.postTransfers,
+    config: {
+      id: `simulator_${__dirname.split('/').pop()}_postTransfers`,
+      tags: tags,
+      auth: null,
+      description: 'Transfer API.',
+      payload: {
+        failAction: 'error',
+        output: 'data'
+      },
+      validate: {
+        headers: Joi.object({
+          accept: Joi.string().optional().regex(/application\/vnd.interoperability[.]/),
+          'content-type': Joi.string().required().regex(/application\/vnd.interoperability[.]/),
+          'content-length': Joi.number().max(5242880),
+          date: Joi.date().format('ddd, D MMM YYYY H:mm:ss [GMT]').required(),
+          'x-forwarded-for': Joi.string().optional(),
+          'fspiop-source': Joi.string().required(),
+          'fspiop-destination': Joi.string().required(),
+          'fspiop-encryption': Joi.string().optional(),
+          'fspiop-signature': Joi.string().optional(),
+          'fspiop-uri': Joi.string().optional(),
+          'fspiop-http-method': Joi.string().optional(),
+          traceparent: Joi.string().optional(),
+          tracestate: Joi.string().optional()
+        }).unknown(false).options({ stripUnknown: true }),
+        payload: Joi.object({
+          transferId: Joi.string().guid().required().description('Id of transfer').label('@ Transfer Id must be in a valid GUID format. @'),
+          payeeFsp: Joi.string().required().min(1).max(32).description('Financial Service Provider of Payee').label('@ A valid Payee FSP number must be supplied. @'),
+          payerFsp: Joi.string().required().min(1).max(32).description('Financial Service Provider of Payer').label('@ A valid Payer FSP number must be supplied. @'),
+          amount: Joi.object().keys({
+            currency: Joi.string().required().currency().description('Currency of the transfer').label('@ Currency needs to be a valid ISO 4217 currency code. @'),
+            amount: Joi.string().required().regex(/^([0]|([1-9][0-9]{0,17}))([.][0-9]{0,3}[1-9])?$/).description('Amount of the transfer')
+          }).required().description('Amount of the transfer').label('@ Supplied amount fails to match the required format. @'),
+          ilpPacket: Joi.string().required().regex(/^[A-Za-z0-9-_]+[=]{0,2}$/).min(1).max(32768).description('ilp packet').label('@ Supplied ILPPacket fails to match the required format. @'),
+          condition: Joi.string().required().trim().max(48).regex(/^[A-Za-z0-9-_]{43}$/).description('Condition of transfer').label('@ A valid transfer condition must be supplied. @'),
+          expiration: Joi.string().required().regex(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:(\.\d{3}))(?:Z|[+-][01]\d:[0-5]\d)$/).description('When the transfer expires').label('@ A valid transfer expiry date must be supplied. @'),
+          extensionList: Joi.object().keys({
+            extension: Joi.array().items(Joi.object().keys({
+              key: Joi.string().required().min(1).max(32).description('Key').label('@ Supplied key fails to match the required format. @'),
+              value: Joi.string().required().min(1).max(128).description('Value').label('@ Supplied key value fails to match the required format. @')
+            })).required().min(1).max(16).description('extension')
+          }).optional().description('Extension list')
+        }),
+        failAction: (request, h, err) => { throw err }
+      }
+    }
+  },
+  {
     method: 'PUT',
     path: '/payerfsp/quotes/{id}',
     handler: Handler.putQuotesById,
